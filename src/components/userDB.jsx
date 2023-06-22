c55cb08dc978891da6a8a703318afa7eeb30b699
sunshine-web-coder <webdev4gaming@gmail.com>
2023-06-22 19:37:47 +0100

    first commit

---
+++ src/components/userDB.jsx
@@ -0,0 +1,14 @@
+const users = [
+  { 
+    id: 1,
+    name: "Doug Brook",
+    cardName: "Doug Brook",
+    username: "brookflora125",
+    password: "brookflora12345",
+    amount: "37,800,000.00",
+  },
+];
+export default users;
+
+
+
